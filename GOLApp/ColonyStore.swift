abe430b74a7e7fc6d929d8e84af028cacf13f330
yanivam <Yaniv@macbook.newton.edu>
2016-11-28 11:57:08 -0500

    Secondary commit

--- GOLApp/ColonyStore.swift
+++ GOLApp/ColonyStore.swift
@@ -37,5 +37,5 @@ class ColonyStore {
         return newTableColonyItem
     
     }
-    //
+    ////
 }
--- GOLApp/DetailViewController.swift
+++ GOLApp/DetailViewController.swift
@@ -49,5 +49,5 @@ class DetailViewController: UIViewController {
         tableColonyItem.templateSetter()
         tableColonyItem.name = namingTextField.text ?? ""
     }
-    //
+    ////
 }
